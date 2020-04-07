2fa2654edfeae544235b75c9276bcee256d3e6c0
kazuya sukigara <sukigarakazuya@sukigarakazuyanoMacBook-ea.local>
2020-04-07 18:47:14 +0900

    capter6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,18 +1,29 @@
 class TodolistsController < ApplicationController
+
   def new
   	@list =List.new
   end
-def create
+ def create
         # ストロングパラメーターを使用
-         list = List.new(list_params)
+    list = List.new(list_params)
         # DBへ保存する
-         list.save
+     list.save
         # トップ画面へリダイレクト
-        redirect_to '/top'
-    end
-    private
-   
+     redirect_to todolist_path(list.id)
+  end
+
+  def index
+          @lists = List.all
+  end
+
+  def show
+    	@list = List.find(params[:id])
+  end
+
+  private
     def list_params
-        params.require(:list).permit(:title, :body)
+          params.require(:list).permit(:title, :body)
     end
-end
+  end
+
+
