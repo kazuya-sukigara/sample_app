cad85a395f94c12f87686f6d3bd0aeb0f4dca5a9
kazuya sukigara <sukigarakazuya@sukigarakazuyanoMacBook-ea.local>
2020-04-08 14:31:27 +0900

    capter8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '97c769d3d20baed570995b5213fabfa5f6154ccb1db9d4c594c62aa7a8bd288dba859a746c88327d16cb9c6cb1f8964490fde15f031b86b8a360d3d35a97d56b'
